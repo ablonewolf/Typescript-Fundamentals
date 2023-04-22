7548525bb09890d585f1fe245ed7bfa0d38b5191
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-04-22 14:40:57 +0600

    demonstrated various types of variables and data types and verified if they are strongly typed.

---
+++ variables/sampleTypes.ts
@@ -0,0 +1,14 @@
+let found = true;
+let grade = 90;
+let firstName = 'Arka';
+let lastName = 'Bhuiyan';
+
+if (found) {
+  console.log(`The grade is ${grade}`);
+  console.log(`Hello ${firstName} ${lastName}`);
+}
+console.log(`Type of variable found is: ${typeof found}`);
+console.log(`Type of variable grade is: ${typeof grade}`);
+console.log(
+  `Type of variables firstName and lastName are: ${typeof firstName} and ${typeof lastName}`
+);
